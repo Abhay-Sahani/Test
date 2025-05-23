04be0c9c56805e51ac958e18c5789bf26634d111
Abhay-Sahani <121797917+Abhay-Sahani@users.noreply.github.com>
2025-05-23 14:50:50 +0530

    Update test_abhay.py

--- test_abhay.py
+++ test_abhay.py
@@ -11,43 +11,4 @@ Enter "help" below or click "Help" above for more information.
 ...         print("Higher!")
 ...     elif guess > number:
 ...         print("Lower!")
-... 
-...         
-Guess the number between 1 and 100: 5
-Higher!
-Guess the number between 1 and 100: 16
-Higher!
-Guess the number between 1 and 100: 80
-Lower!
-Guess the number between 1 and 100: 50
-Higher!
-Guess the number between 1 and 100: 
-Traceback (most recent call last):
-  File "<pyshell#11>", line 2, in <module>
-    guess = int(input("Guess the number between 1 and 100: "))
-ValueError: invalid literal for int() with base 10: ''
->>> print(f"Correct! You guessed the number in {attempts} attempts.")
-Correct! You guessed the number in 4 attempts.
->>> import random
-... number = random.randint(1, 100)
-... guess = None
-... attempts = 0
-... while guess != number:
-...     guess = int(input("Guess the number between 1 and 100: "))
-...     attempts += 1
-...     if guess < number:
-...         print("Higher!")
-...     elif guess > number:
-...         print("Lower!")
-... 
-SyntaxError: multiple statements found while compiling a single statement
->>> import random
-... number = random.randint(1, 100)
-... guess = None
-... attempts = 0
-... while guess != number:
-...     guess = int(input("Guess the number between 1 and 100: "))
-...     attempts += 1
-...     if guess < number:
-...         print("Higher!")
-...     elif guess > number:
+print(f"Correct! You guessed the number in {attempts} attempts.")
